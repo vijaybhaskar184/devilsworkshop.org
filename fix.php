6d11dadc3d58e23c22437ee57727d26a4b51fa10
Rahul Bansal <rahul.bansal@rtcamp.com>
2015-05-03 23:56:48 +0530

    changes

--- fix.php
+++ fix.php
@@ -1,55 +1,53 @@
 <?php
-$authors = glob('data/authors/*.toml');
+//glob markdown files
+$files = glob('content/posts/*.md');
+foreach($files as $file) {
+  $base = basename($file);
+  // $cleanfile = sanitize_file_name($base);
+  $cleanfile = preg_replace('/[^\00-\255]+/u', '', $file);
 
-foreach($authors as $author) {
-    $username =  basename($author,".toml");
-    $info = parse_ini_file($author);
-    $display =  $info['display_name'];
+  // $result = preg_match_all('/(\d\d\d\d)-(\d\d)-(\d\d)-(.*)/i', $cleanfile, $matches);
 
-    $oldusername = $username;
-    //check username for @ (email)
-    if(strpos($username, "@")){
-      echo "before email: $username \n";
-      $username = explode('@',$username)[0];
-      // $username = left($email, stripos($email, '@'));
-      echo "after email: $username \n";
-    }
+  if($file != $cleanfile){
+    var_dump($file);
+    var_dump($cleanfile);
+    rename($file, $cleanfile);
+  }
 
-    //check username for dot .
-    if(strpos($username, ".")){
-      echo "before dot: $username \n";
-      $username = str_replace(".", "", $username);
-      echo "after dot: $username \n";
-    }
+  // echo rename($cleanfile, );
 
-    //rename author logic
-    if ($oldusername != $username) {
-      echo rename($author,dirname($author) . '/' . $username . '.toml');
-    }
+  // // DEBUG
+  // // var_dump($content);
+  // var_dump($result);
+  // var_dump($matches[2][0]);
+  // var_dump($master[$matches[2][0]]);
+  // foreach ($matches as $val) {
+  //   print_r($val);
+  // }
+  // $new = $master[$matches[2][0]];
+  //
+  // //if displayname and username is same, skip this
+  // if($matches[2][0] == $new)
+  //   continue;
+  //
+  // $result = preg_replace('/(author:\s)(.*)/m', "author: $new",$content);
+  // // $diff = xdiff_string_diff($content, $result, 1);
+  // // var_dump($diff);
+  // // var_dump($content);
+  // // var_dump($result);
+  //
+  // file_put_contents($file, $result);
+}//end of foreach
 
-    // continue;
-    // echo $author ."\n";
-    // echo $username."\n";
-    // echo $display ."\n";
+function sanitize_file_name( $filename ) {
+	$filename_raw = $filename;
+	$special_chars = array("?", "[", "]", "/", "\\", "=", "<", ">", ":", ";", ",", "'", "\"", "&", "$", "#", "*", "(", ")", "|", "~", "`", "!", "{", "}", chr(0));
+	$filename = preg_replace( "#\x{00a0}#siu", ' ', $filename );
+	$filename = str_replace( $special_chars, '', $filename );
+	$filename = str_replace( array( '%20', '+' ), '-', $filename );
+	$filename = preg_replace( '/[\r\n\t -]+/', '-', $filename );
+	$filename = trim( $filename, '.-_' );
 
-    if($username == $display){
-      echo "Replacement start \n";
-      var_dump($author);
-      var_dump($username);
-      var_dump($display);
-
-      $files = glob('content/posts/*.md');
-      foreach($files as $file) {
-        file_put_contents($file, str_replace('author: ' . $display, 'author: ' . $username, file_get_contents($file)));
-      }
-    }
-
+	return $filename;
 }
-// exit;
-// // replace
-// // $files = glob('content/posts/*.md');
-// foreach($files as $file) {
-//     // echo $file . "\n";
-// }
-
 ?>
